f63948d1e434a26ff2733f5920978d5a9c2f90c5
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2024-11-30 15:13:00 +0530

    Navigation bar creatin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RecipiesComponent } from "./recipies/recipies.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RecipiesComponent, ShoppingListComponent],
+  imports: [RouterOutlet, RecipiesComponent, ShoppingListComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
