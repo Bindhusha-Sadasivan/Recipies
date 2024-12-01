a751e223080f5c978eb848eda2539c947344aaa8
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2024-12-01 20:47:05 +0530

    Created Recipie details and recioie description

--- src/app/recipies/recipies-list/recipie-list.component.ts
+++ src/app/recipies/recipies-list/recipie-list.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RecipieItemComponent } from "./recipies-item/recipie-item.component";
+import { Recipie } from '../../Model/recipie.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipie-list',
   standalone: true,
-  imports: [RecipieItemComponent],
+  imports: [RecipieItemComponent, CommonModule],
   templateUrl: './recipie-list.component.html',
   styleUrl: './recipie-list.component.css'
 })
 export class RecipieListComponent {
-
+recipie: Recipie[] = [
+  new Recipie("A Test Recipie", "Description of a Test Recipie", "https://c8.alamy.com/comp/HW5KR2/cookies-forming-the-word-recipes-HW5KR2.jpg"),
+  new Recipie("A Test Recipie", "Description of a Test Recipie", "https://c8.alamy.com/comp/HW5KR2/cookies-forming-the-word-recipes-HW5KR2.jpg"),
+  new Recipie("A Test Recipie", "Description of a Test Recipie", "https://c8.alamy.com/comp/HW5KR2/cookies-forming-the-word-recipes-HW5KR2.jpg")
+];
 }
