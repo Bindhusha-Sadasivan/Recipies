b2a84631d1941275a9184ba2876326c782e4caa5
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2024-12-28 16:09:42 +0530

    166 - Retrieving Route Parameters

--- src/app/recipies/recipie-edit/recipie-edit.component.ts
+++ src/app/recipies/recipie-edit/recipie-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipie-edit',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
   templateUrl: './recipie-edit.component.html',
   styleUrl: './recipie-edit.component.css'
 })
-export class RecipieEditComponent {
+export class RecipieEditComponent implements OnInit{
+
+  id!:number;
+  editMode:boolean = false;
+
+  constructor(
+    private route: ActivatedRoute
+  ){
+
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    // const id = this.route.snapshot.params['id'];
+
+    this.route.params.subscribe(
+      (params:Params) => {
+        this.id = params['id'];
+        this.editMode = params['id'] != null;
+        console.log(this.editMode)
+      }
+    )
+  }
 
 }
