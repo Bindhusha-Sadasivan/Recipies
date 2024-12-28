973b4d94d5a7150d46a22f84a44a8dbe3ed14ee4
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2024-12-28 15:37:10 +0530

    163 - Passing Dynamic Parameters to Links

--- src/app/recipies/recipies-list/recipies-item/recipie-item.component.ts
+++ src/app/recipies/recipies-list/recipies-item/recipie-item.component.ts
@@ -2,17 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Recipie } from '../../../Model/recipie.model';
 import { RecipiesService } from '../../recipies.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-recipie-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './recipie-item.component.html',
   styleUrl: './recipie-item.component.css'
 })
 export class RecipieItemComponent {
 @Input() receivedRecipie!:Recipie;
 // @Output() selectedRecipie = new EventEmitter<any>();
+@Input() index!:number;
 
 
 constructor(private recipiesService:RecipiesService) {
