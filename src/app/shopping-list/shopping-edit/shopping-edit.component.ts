1ec9659e6744d5709e20bc08c40b1c33a240c51c
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2025-02-24 06:07:22 +0530

    219 - TD Adding the Shopping List Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,11 +1,13 @@
 import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { Ingredients } from '../../Model/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './shopping-edit.component.html',
   styleUrl: './shopping-edit.component.css'
 })
@@ -28,13 +30,18 @@ constructor(private shoppingListService:ShoppingListService) {
 
 }
 
-onAdd(){
-  const enterdName:string = this.enterdName.nativeElement.value;
-  const enteredAmount:number = this.enteredAmount.nativeElement.value;
-  const newIngredient = new Ingredients(enterdName, enteredAmount);
-  // this.shoppingListService.enteredIngredients.emit(newIngredient);
-  // this.shoppingListService.enteredIngredients.subscribe(data => console.log(data));
-  this.shoppingListService.addedIngredients(newIngredient);
-}
+// onAdd(){
+//   const enterdName:string = this.enterdName.nativeElement.value;
+//   const enteredAmount:number = this.enteredAmount.nativeElement.value;
+//   const newIngredient = new Ingredients(enterdName, enteredAmount);
+//   // this.shoppingListService.enteredIngredients.emit(newIngredient);
+//   // this.shoppingListService.enteredIngredients.subscribe(data => console.log(data));
+//   this.shoppingListService.addedIngredients(newIngredient);
+// }
 
+onSubmit(form:NgForm){
+const value = form.value;
+const newIngredient = new Ingredients(value.name, value.amount);
+this.shoppingListService.addedIngredients(newIngredient);
+}
 }
