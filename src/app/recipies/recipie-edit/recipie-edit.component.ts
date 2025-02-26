3ea1946a8e0fe0a50cec53a5400094b9226b08ef
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2025-02-26 22:47:23 +0530

    228 - Creating the Form For Editing Recipes

--- src/app/recipies/recipie-edit/recipie-edit.component.ts
+++ src/app/recipies/recipie-edit/recipie-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipiesService } from '../recipies.service';
 
 @Component({
   selector: 'app-recipie-edit',
@@ -12,12 +14,12 @@ export class RecipieEditComponent implements OnInit{
 
   id!:number;
   editMode:boolean = false;
+  recipieForm!: FormGroup;
 
   constructor(
-    private route: ActivatedRoute
-  ){
-
-  }
+    private route: ActivatedRoute,
+    private recipieService: RecipiesService
+  ){}
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
@@ -28,9 +30,29 @@ export class RecipieEditComponent implements OnInit{
       (params:Params) => {
         this.id = params['id'];
         this.editMode = params['id'] != null;
-        console.log(this.editMode)
+        console.log(this.editMode);
+        this.initForm();
       }
     )
+  }
+
+  private initForm(){
+    let recipieName = '';
+    let recipieImagePath = '';
+    let recipieDescription = '';
+
+    if(this.editMode){
+      const recipie = this.recipieService.getRecipiesById(this.id);
+      recipieName = recipie.name;
+      recipieImagePath = recipie.imagePath;
+      recipieDescription = recipie.description;
+    }
+
+    this.recipieForm = new FormGroup({
+      'name': new FormControl(recipieName),
+      'imagePath': new FormControl(recipieImagePath),
+      'description': new FormControl(recipieDescription)
+    });
   }
 
 }
