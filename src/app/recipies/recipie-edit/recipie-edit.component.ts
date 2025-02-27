59fe8eaa8fc5e3c06bcea503c8f19a83893f042c
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2025-02-27 22:58:13 +0530

    231 - Adding Ingredient Controls to a Form Array

--- src/app/recipies/recipie-edit/recipie-edit.component.ts
+++ src/app/recipies/recipie-edit/recipie-edit.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipiesService } from '../recipies.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipie-edit',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './recipie-edit.component.html',
   styleUrl: './recipie-edit.component.css'
 })
@@ -36,24 +37,41 @@ export class RecipieEditComponent implements OnInit{
     )
   }
 
-  private initForm(){
+   private initForm(){
     let recipieName = '';
     let recipieImagePath = '';
     let recipieDescription = '';
+    let recipieIngredients:any = new FormArray([]);
 
     if(this.editMode){
       const recipie = this.recipieService.getRecipiesById(this.id);
       recipieName = recipie.name;
       recipieImagePath = recipie.imagePath;
       recipieDescription = recipie.description;
+      if(recipie['ingredients']){
+        for(let ingredients of recipie.ingredients){
+          recipieIngredients.push(
+            new FormGroup({
+              'name': new FormControl(ingredients.name),
+              'amount': new FormControl(ingredients.amount)
+            })
+          )
+        }
     }
+  }
 
     this.recipieForm = new FormGroup({
       'name': new FormControl(recipieName),
       'imagePath': new FormControl(recipieImagePath),
-      'description': new FormControl(recipieDescription)
+      'description': new FormControl(recipieDescription),
+      'ingredients': recipieIngredients
     });
   }
+
+  get recipieFormControls(){
+    return (<FormArray>this.recipieForm.get('ingredients')).controls;
+  }
+
   onSubmit(){
     console.log(this.recipieForm);
   }
