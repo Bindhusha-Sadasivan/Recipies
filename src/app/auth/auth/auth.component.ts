fc1ebafb90492d8345a092a3cdabedb5258a2c61
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2025-03-22 15:15:08 +0530

    296 - Adding a Loading Spinner Error Handling Logic

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { CommonModule } from '@angular/common';
+import { LoadingSpinnerComponent } from '../../shared-service/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule,LoadingSpinnerComponent],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
   isLoggedIn:boolean = true;
+  isLoading:boolean = false;
+  error!:string;
 
   constructor(private authservice:AuthService){}
 
@@ -24,6 +28,7 @@ export class AuthComponent {
     if(!authForm.valid){
       return
     }
+    this.isLoading = true;
     const email = authForm.value.email;
     const password = authForm.value.password;
     if(this.isLoggedIn){
@@ -33,6 +38,12 @@ export class AuthComponent {
       this.authservice.signup(email,password).subscribe({
         next: (response:any) => {
           console.log("Response:", response)
+          this.isLoading = false;
+        },
+        error: (error:any) => {
+          console.log("Error:", error)
+          this.error=error.message;
+          this.isLoading = false;
         }
       })
     }
