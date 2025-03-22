1f01834fcf928fc348e1e1450054644a8ae8cf35
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2025-03-22 13:44:10 +0530

    294 - Preparing the Signup Request

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { authResponse } from './interfaces/authResponcse.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http:HttpClient) { }
+
+  signup(email:string, password:string){
+
+    this.http.post<authResponse>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyABNkTVMJuGL04SzxkoX92MgnIGHpTPTU0',
+      {
+        email:email,
+        password:password,
+        returnSecureToken:true
+      }
+    )
+  }
+}
---
+++ src/app/auth/interfaces/authResponcse.interface.ts
@@ -0,0 +1,7 @@
+export interface authResponse {
+  idToken:string	//A Firebase Auth ID token for the newly created user.
+  email:string	//The email for the newly created user.
+  refreshToken:string	//A Firebase Auth refresh token for the newly created user.
+  expiresIn:string	//The number of seconds in which the ID token expires.
+  localId:string	//The uid of the newly created user.
+}
